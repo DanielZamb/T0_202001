e50b4e9094a6ce4256ec48f4c7456f4248726002
Daniel Zambrano <daniel.ernesto.zambrano@gmail.com>
2020-01-24 13:51:49 -0500

    Cambios echos en la Interfaz de arreglo dinamico

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -7,7 +7,7 @@ package model.data_structures;
  * @author Fernando De la Rosa
  *
  */
-public class ArregloDinamico implements IArregloDinamico {
+public class ArregloDinamico<Integer extends Comparable<Integer>> implements IArregloDinamico<Integer> {
 		/**
 		 * Capacidad maxima del arreglo
 		 */
